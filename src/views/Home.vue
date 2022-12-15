2e14095cfad15b35fcc69c5f4b9b911e9a276516
Roy <roy.derks@atalio.com>
2022-12-15 20:40:17 +0000

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <HelloWorld
+    msg="Only now, you can get all premium features & more with the best discounts of the year."
+  />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from "../components/HelloWorld.vue";
+</script>
